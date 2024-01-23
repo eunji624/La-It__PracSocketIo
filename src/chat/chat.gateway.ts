8f78d0c62122c9862844562ef3c2affad63cb6d1
eunji624 <allrkatk@naver.com>
2024-01-23 13:39:04 +0900

    feat: 채팅 금칙어 기능 추가

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -38,11 +38,22 @@ export class ChatGateway {
         console.log('=====>', filterWord);
         if (filterWord) {
             return this.server.to(client.id).emit('alert', '허용하지 않는 단어입니다.');
-        } else {
-            const saveChat = await this.chatService.createChat(client, value, liveId, userId, nickname);
-            console.log('saveChat', saveChat, client.handshake.auth.user.nickname);
-            return this.server.to(liveId).emit('sending_message', saveChat.content, nickname);
         }
+        //유저가 30초동안 20개 이상의 메세지를 보내면 채팅 1분간 정지.
+        //데이터베이스에서 확인.
+        //현재 요청 시점 기준으로 유저아이디에 해당하는 유저의 데이터가 20개 이상일때/
+        //근데 이렇게 하면 너무 많은 조회 발생.
+        //30초 간격으로 유저가 메세지 보낼때 변수에 count해주고
+        //30초가 지나면
+
+        //정지는 정지가 발생한 시점으로부터 timeout걸어서 1분간 alert발생(채팅 도배로 잠시 채팅을 입력할 수 없습니다)
+        //정지당했던 유저가 또다시 30초 동안 20개 이상의 메세지를 보내면 3분간 정지
+        //또다시 보내면 5분간 정지
+
+        //
+        const saveChat = await this.chatService.createChat(client, value, liveId, userId, nickname);
+        console.log('saveChat', saveChat, client.handshake.auth.user.nickname);
+        return this.server.to(liveId).emit('sending_message', saveChat.content, nickname);
     }
 
     @SubscribeMessage('get_all_chat_by_liveId')
